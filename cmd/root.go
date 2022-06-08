1717df4d5c9bbb3ae639939999800868a3e5efcd
Roger Gomez <rgomez@presidio.com>
2022-06-08 17:11:58 -0400

    adding description use case for the main command.

--- cmd/root.go
+++ cmd/root.go
@@ -40,13 +40,7 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "go-aws-routing",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Extracts all routing information from AWS and save it to a CSV file",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
