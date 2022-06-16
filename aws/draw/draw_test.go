089e34d30a06583b1656c02c543166f1e5dde095
Roger Gomez <rgomez@presidio.com>
2022-06-16 17:08:05 -0400

    adding test cases for the drawing.

--- aws/draw/draw_test.go
+++ aws/draw/draw_test.go
@@ -1,16 +1,40 @@
- package draw
+package draw
 
 import (
 	"io/fs"
+	"os"
 	"reflect"
 	"testing"
 
 	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
 	"gitlab.presidio.com/rgomez/aws-router/aws/awsrouter"
 	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
+var tgw = awsrouter.Tgw{
+	Name: "TestDrawTgw",
+	ID:   "TestDrawTgw",
+	RouteTables: []*awsrouter.TgwRouteTable{
+		{
+			Name: "TestDrawTgw1",
+			ID:   "tgw-rtb-042e9098a139d16f3",
+		},
+		{
+			Name: "TestDrawTgw2",
+			ID:   "tgw-rtb-042e9098a139d16f4",
+		},
+		{
+			Name: "TestDrawTgw3",
+			ID:   "tgw-rtb-042e9098a139d16f5",
+		},
+	},
+}
+
 func TestDrawTgwRouteTable(t *testing.T) {
+	fontt, _ := truetype.Parse(goregular.TTF)
+	face := truetype.NewFace(fontt, &truetype.Options{Size: 12})
 	type args struct {
 		dc   *gg.Context
 		x    float64
@@ -22,7 +46,16 @@ func TestDrawTgwRouteTable(t *testing.T) {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "TestDrawTgwRouteTable",
+			args: args{
+				dc:   gg.NewContext(tgwWidth, tgwHeight),
+				x:    0,
+				y:    0,
+				name: "TestDrawTgwRouteTable",
+				face: face,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -42,7 +75,15 @@ func TestDrawTgw(t *testing.T) {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "TestDrawTgw",
+			args: args{
+				dc:  gg.NewContext(tgwWidth, tgwHeight),
+				x:   0,
+				y:   0,
+				tgw: tgw,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -59,9 +100,7 @@ func TestCreateTgwContext(t *testing.T) {
 		name string
 		args args
 		want *gg.Context
-	}{
-		// TODO: Add test cases.
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := CreateTgwContext(tt.args.tgw); !reflect.DeepEqual(got, tt.want) {
@@ -72,6 +111,7 @@ func TestCreateTgwContext(t *testing.T) {
 }
 
 func TestDrawTgwFull(t *testing.T) {
+	folder, _ := os.Stat("testdata")
 	type args struct {
 		tgw    awsrouter.Tgw
 		folder fs.FileInfo
@@ -81,7 +121,34 @@ func TestDrawTgwFull(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "NoFolder",
+			args: args{
+				tgw:    tgw,
+				folder: nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "NoRouteTables",
+			args: args{
+				tgw: awsrouter.Tgw{
+					Name:        "TestDrawTgw",
+					ID:          "TestDrawTgw",
+					RouteTables: []*awsrouter.TgwRouteTable{},
+				},
+				folder: nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "Working",
+			args: args{
+				tgw:    tgw,
+				folder: folder,
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
