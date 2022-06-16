0bf52dd356e0b4ede354ca942a0059c23cad51a1
Roger Gomez <rgomez@presidio.com>
2022-06-16 11:37:27 -0400

    Cobra CLI option for the drawing.

---
+++ cmd/draw.go
@@ -0,0 +1,86 @@
+/*
+Copyright © 2022 Roger Gomez rogerscuall@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/spf13/cobra"
+	"gitlab.presidio.com/rgomez/aws-router/aws/awsrouter"
+	"gitlab.presidio.com/rgomez/aws-router/aws/draw"
+)
+
+// drawCmd represents the draw command
+var drawCmd = &cobra.Command{
+	Use:   "draw",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		defer func() {
+			if err != nil {
+				cobra.CheckErr(err)
+			}
+		}()
+		fmt.Println("Downloading routing information from AWS")
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		client := ec2.NewFromConfig(cfg)
+		tgws, err := awsrouter.UpdateRouting(context.TODO(), client)
+		folder, err := os.Stat("drawings")
+		// if folder does not exist, create it
+		if os.IsNotExist(err) {
+			err = os.Mkdir("drawings", 0755)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		for _, tgw := range tgws {
+			if err := draw.DrawTgwFull(*tgw, folder); err != nil {
+				fmt.Println("Error drawing tgw: ", tgw.Name)
+				fmt.Println(err)
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(drawCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// drawCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// drawCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
