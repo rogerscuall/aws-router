876815a470ce946cfa235e555381bc419d918398
Roger Gomez <rgomez@presidio.com>
2022-06-20 17:40:00 -0400

    File for the TGW path.

---
+++ aws/awsrouter/transit_gateway_path.go
@@ -0,0 +1,20 @@
+package awsrouter
+
+// TgwPath represent the path through the Transit Gateway, from the source to the destination.
+type TgwPath struct {
+	Source           TgwAttachment
+	Destination      TgwAttachment
+	TransitGatewayID string
+	Path             []TgwRouteTable
+}
+
+// func (t *TransitGatewayPath) FindSourceAttachment(sourceIPAddress net.IP) TgwAttachment {
+// 	for _, routeTable := range tgw.RouteTables {
+// 		fmt.Println("routeTable:", routeTable.ID)
+// 		result, err := routeTable.BestRouteToIP(sourceIPAddress)
+// 		if err != nil {
+// 			cobra.CheckErr(err)
+// 		}
+// 		fmt.Println("result:", *result.DestinationCidrBlock)
+// 	}
+// }
