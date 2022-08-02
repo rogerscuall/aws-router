653a2c2ba04ede0658084ceca5688099379fc1bb
Roger Gomez <rgomez@presidio.com>
2022-08-02 11:28:30 -0400

    Update to include call to the STS identity.

--- aws/auth/auth.go
+++ aws/auth/auth.go
@@ -2,20 +2,27 @@ package auth
 
 import (
 	"context"
+	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 func GetClient() (client *ec2.Client, err error) {
 	ctx := context.TODO()
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		return nil, err
+		return nil, ErrNoDefaultAuthentication
 	}
+
+	stsClient := sts.NewFromConfig(cfg)
+	identity, err := stsClient.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
+	if err != nil {
+		return nil, ErrStsIdentityNotFound
+	}
+	log.Printf("Account: %s, Arn: %s", aws.ToString(identity.Account), aws.ToString(identity.Arn))
 	client = ec2.NewFromConfig(cfg)
 	return client, nil
-	// clientSTS := sts.NewFromConfig(cfg)
-	// 	identity, err := clientSTS.GetCallerIdentity(context.TODO(), &sts.GetCallerIdentityInput{})
-	//fmt.Printf("Account: %s, Arn: %s", aws.ToString(identity.Account), aws.ToString(identity.Arn))
 }
--- aws/auth/errors.go
+++ aws/auth/errors.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrNoDefaultAuthentication = errors.New("no default authentication was found")
+	ErrStsIdentityNotFound     = errors.New("sts identity not found")
 )
